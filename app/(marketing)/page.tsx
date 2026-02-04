22681b25ac3f281b6e91db896f4b30e00f329b81
bentleyminwa <bentleyminwa@gmail.com>
2026-02-04 11:35:59 +0300

    merge conflicts

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button';
-import { default as Image, default as Image } from 'next/image';
 import Link from 'next/link';
 
 export default function LandingPage() {
